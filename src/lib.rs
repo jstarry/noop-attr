14b4e2a1cc9c57a2cf9957ec72fdb441452ec479
Samuel Moelius <sam@moeli.us>
2022-09-20 09:18:52 -0400

    Add `no-arguments` feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 extern crate proc_macro;
 
+#[allow(unused_variables)]
 #[proc_macro_attribute]
-pub fn noop(_: proc_macro::TokenStream, body: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn noop(
+    attr: proc_macro::TokenStream,
+    body: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    #[cfg(feature = "no-arguments")]
+    assert!(attr.is_empty(), "this attribute takes no arguments");
     body
 }
